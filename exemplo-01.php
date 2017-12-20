e579d24e5e7e46ab5a3ae46a25d0aeed9821d90c
Neto Sales <contato@netosales.com.br>
2017-12-20 20:29:47 -0300

    Arquivos da aula 93.

---
+++ exemplo-01.php
@@ -0,0 +1,18 @@
+<form method="post">
+	<input type="text" name="busca">
+	<button type="submit">Enviar</button>
+</form>
+<?php
+$_POST['busca'] = '<a href="#"><strong>Oi</strong></a><script>alert("Ok")</script>';
+
+if(isset($_POST['busca'])){
+
+
+	//echo strip_tags($_POST['busca'], "<strong><a>");
+	echo htmlentities($_POST['busca']);
+
+}
+
+
+
+?>
